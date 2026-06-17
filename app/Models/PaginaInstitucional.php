f41af96ff8782613c57ee9f73669f201dec2160c
Dayana Rojas <168503064+dayanarojasdrp@users.noreply.github.com>
2026-06-17 15:27:48 -0400

    Add traceability to paginas

--- app/Models/PaginaInstitucional.php
+++ app/Models/PaginaInstitucional.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Concerns\RecordsActivity;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PaginaInstitucional extends Model
 {
+    use RecordsActivity, SoftDeletes;
+
     protected $table = 'paginas_institucionales';
 
     protected $fillable = [
