b1d0ab81bb80c593b30a90ee92a96627ab96b2da
Dayana Rojas <168503064+dayanarojasdrp@users.noreply.github.com>
2026-06-17 15:27:15 -0400

    Add traceability to ministerios

--- app/Models/Ministerio.php
+++ app/Models/Ministerio.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Concerns\RecordsActivity;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Ministerio extends Model
 {
+    use RecordsActivity, SoftDeletes;
+
     protected $fillable = [
         'nombre',
         'slug',
