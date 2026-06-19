15013439eeb69dad4d2fb23983b73bdf0f19ecb8
Dayana Rojas <168503064+dayanarojasdrp@users.noreply.github.com>
2026-06-19 13:27:27 -0400

    Changes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/run-migrations-secret-2026', function () {
+    \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+    $migrateOutput = \Illuminate\Support\Facades\Artisan::output();
+
+    \Illuminate\Support\Facades\Artisan::call('db:seed', ['--force' => true]);
+    $seedOutput = \Illuminate\Support\Facades\Artisan::output();
+
+    return '<pre>' . $migrateOutput . "\n\n" . $seedOutput . '</pre>';
+});
